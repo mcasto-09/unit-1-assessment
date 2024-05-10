4e50391844f2be4531b78a56e41439e3c1eda793
Mary Castorani <mgcastorani@gmail.com>
2024-05-10 12:23:18 -0600

    wb assess done, functions almost done

--- src/functions.js
+++ src/functions.js
@@ -2,41 +2,60 @@
 // Ex.:
 //   divide(10, 2);
 //   => 5
-function divide(x, y) {}
-
+function divide(x, y) {
+  return x/y;
+}
+console.log(divide(x,y))
 // Return the average of x and y.
 // Here, the average of x and y is the *mean* of x and y. In other words, it's
 // computed by dividing the sum of x and y by 2.
 // Ex.:
 //   average(10, 2);
 //   => 6
-function average(x, y) {}
+function average(x, y) {
+    let z = (x + y)
+    return z / 2
+  }
+  
+  console.log(average(x,y)) 
 
 // Return true if x and y are mostly equivalent up to a tolerance of 0.001.
 // In other words, return true if the aboslute value of x - y is less than 0.001.
 // Ex.:
 //   approximatelyEqual(10.001, 10);
 //   => true
-function approximatelyEqual(x, y) {}
-
+function approximatelyEqual(x, y) {
+  if (x - y < 0.001) {
+  return true
+  } else {
+    return false
+  }
+}
 // Given a first name and last name, return a full name in the format "FIRST LAST"
 // Ex.:
 //   fullName('John', 'Doe');
 //   => 'John Doe'
-function fullName(firstName, lastName) {}
+function fullName(firstName, lastName) {
+  return (`${firstName} ${lastName}`);
+}
 
 // Generate the sentence "PERSON was drinking BEVERAGE at LOCATION" using the
 // person, beverage and location provided.
 // Ex.:
 //   generateSentence('Kay', 'coffee', 'the local cafe');
 //   => 'Kay was drinking coffee at the local cafe.'
-function generateSentence(person, beverage, location) {}
+function generateSentence(person, beverage, location) {
+  return `${person} was drinking ${beverage} at ${location}`
+}
 
 // Return the given string with all vowels replced with '*' characters.
 // Ex.:
 //   censorVowels('javascript');
 //   => 'j*v*scr*pt'
-function censorVowels(string) {}
+function censorVowels(string) {
+  let vowels = ("a", "e","i", "o", "u")
+  return string.replace(vowels, `*`)
+}
 
 // Return the given string in sticky case.
 // Ex.:
